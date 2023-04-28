435938ae2d40cba6a5d93e14b1f127645e08fd05
Zignuts Technolab <ztlab153@Zignutss-Mac-mini.local>
2023-04-28 13:08:50 +0530

    Correct spaling

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,7 +4,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      header: '*',
+      headers: '*',
       origin: ['http://localhost:1337','http://localhost:3000','https://1105-2405-201-2025-e0bb-c86e-f446-905b-ac4e.ngrok-free.app']
     }
   },
--- src/api/customer/content-types/customer/lifecycles.js
+++ src/api/customer/content-types/customer/lifecycles.js
@@ -7,22 +7,22 @@ module.exports = {
   },
   async afterCreate(event){
     // Send email with nodemailer
-    // strapi.plugins["email"].services.email.send({
-    //   to: event.result.email,
-    //   from: "hello@zignuts.com", //e.g. single sender verification in SendGrid
-    //   subject: "welcome email otp",
-    //   html: `<p>hi ${event.result.firstName}, this is your otp ${event.result.otp}</p>`,
-    // });
-    // // Send OTP with twilio
-    // client.messages
-    //   .create({
-    //     body: `Hi
-    //       ${event.result.firstName} 
-    //       this is your OTP 
-    //       ${event.result.otp}`,
-    //     from: "+18883836867", // From a valid Twilio number
-    //     to: "+919427836867",
-    //   })
-    //   .then((message) => console.log(message.sid));
+    strapi.plugins["email"].services.email.send({
+      to: event.result.email,
+      from: "hello@zignuts.com", //e.g. single sender verification in SendGrid
+      subject: "welcome email otp",
+      html: `<p>hi ${event.result.firstName}, this is your otp ${event.result.otp}</p>`,
+    });
+    // Send OTP with twilio
+    client.messages
+      .create({
+        body: `Hi
+          ${event.result.firstName} 
+          this is your OTP 
+          ${event.result.otp}`,
+        from: "+18883836867", // From a valid Twilio number
+        to: "+919427836867",
+      })
+      .then((message) => console.log(message.sid));
   }
 };
